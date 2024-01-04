e17a34283a9186cbc5a61cd385c0b15c99d8633a
summerwind669 <smmerwind669@gmail.com>
2024-01-04 17:15:24 +0800

    修复了下不能查询sbc日志的bug

--- shell/Query log/sbc/query_sbc_ucp_proxylog.sh
+++ shell/Query log/sbc/query_sbc_ucp_proxylog.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#该脚本用于查询5gisbc-signal,sip-proxy,isbc,asbc的信令日志ucp_proxy.log
-#实时日志查询格式bash vm_query_11ivr_OpenVxilog.sh 号码
-#归档日志查询格式bash vm_query_11ivr_OpenVxilog.sh 号码 2024010415
-
 
 # 获取变量1（sbc）、变量2（号码）、变量3（时间）
 sbc="$1"
@@ -20,17 +16,16 @@ case $sbc in
         ;;
 esac
 
-# 检查变量3是否合法（使用正则表达式匹配日期格式）
-if ! [[ $timestamp =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
-    echo "Error: Invalid date format. Use 'yyyy-mm-dd'."
-    exit 1
-fi
-
-# 使用ansible查询日志
+# 判断查询实时日志还是归档日志
 if [ -z "$timestamp" ]; then
     # 如果没有提供时间参数，则执行实时日志查询
     ansible -i ~/hosts/hosts_all "$sbc" -m shell -a "zgrep -h '$number' /data/ucp/fusionsbc/log/kamailio/ucp_proxy.log"
 else
     # 如果提供了时间参数，则执行归档日志查询
+    # 检查变量3是否合法（使用正则表达式匹配日期格式）
+    if ! [[ $timestamp =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
+        echo "Error: Invalid date format. Use 'yyyy-mm-dd'."
+        exit 1
+    fi
     ansible -i ~/hosts/hosts_all "$sbc" -m shell -a "zgrep -h '$number' /data/ucp/fusionsbc/log/kamailio/ucp_proxy.log.$timestamp*"
 fi
