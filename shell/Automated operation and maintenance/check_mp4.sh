52f0d602e771a8eb6358adf351de7da81084a651
summerwind669 <smmerwind669@gmail.com>
2024-04-08 18:57:44 +0800

    新增ffprobe绿色二进制文件位置

--- shell/Automated operation and maintenance/check_mp4.sh
+++ shell/Automated operation and maintenance/check_mp4.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+#运行脚本需要安装ffmpeg,欧拉系统源上有个工具,centos源没有这个工具,使用yum安装即可
 #脚本用法：bash check_mp4.sh 目录路径，比如bash check_mp4.sh /data/ucp/tmp/06 查询6号当天的
+#如果无法安装,请直接把126跳板机上的绿色二进制文件拷贝到record的/usr/bin下，赋权755
+#126文件位置/home/ucp/founder/soft/video_preprocess/ffmepg/ffprobe
 
 # 检查目录是否存在
 if [ ! -d "$1" ]; then
@@ -11,7 +14,7 @@ fi
 # 检查是否存在 /tmp/output.txt 文件，如果存在则删除
 output_file="/tmp/output.txt"
 if [ -f "$output_file" ]; then
-    del "$output_file"
+    rm "$output_file"
 fi
 
 # 使用 find 命令递归地遍历目录及其子目录下的所有 mp4 文件，并对每个文件执行 ffprobe 分析
@@ -29,7 +32,7 @@ echo "总共检查了 $total_mp4_count 个 mp4 文件"
 
 # 计算 mp4 文件的损坏率
 if [ "$total_mp4_count" -ne 0 ]; then
-    file_corruption_rate=$(echo "scale=2; $invalid_data_count / $total_mp4_count * 100" | bc)
+    file_corruption_rate=$(echo "scale=3; $invalid_data_count / $total_mp4_count * 100" | bc | awk '{printf "%.3f", $0}')
     echo "mp4 文件的文件损坏率为: $file_corruption_rate%"
 else
     echo "没有找到任何 mp4 文件"
